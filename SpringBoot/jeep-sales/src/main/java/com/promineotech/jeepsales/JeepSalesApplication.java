7b0335c2d45c95e79047070f084dedbe543c0c6b
Shaun <117416185+shaunbo@users.noreply.github.com>
2023-02-18 18:28:32 -0800

    Add files via upload

---
+++ SpringBoot/jeep-sales/src/main/java/com/promineotech/jeepsales/JeepSalesApplication.java
@@ -0,0 +1,13 @@
+package com.promineotech.jeepsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeepSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeepSalesApplication.class, args);
+	}
+
+}
